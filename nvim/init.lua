43f37063eb05d012537addc4693126c898ec057a
Brayan Diaz <brayandiazssjd@gmail.com>
2024-12-15 20:41:34 -0500

    First commit.

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+require "keymaps"
+require "lazy-config"
+require "nvim-tree-config"
+require "lsp-config"
+require "whichkey"
+require "options"
+require("toggleterm-config")
+require("autopairs-config")
+require("lualine-config")
+require("bufferline-config")
